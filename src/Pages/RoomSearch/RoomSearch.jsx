bfb2b218f5a09480eaed1049953fbfc4db83f1af
wzr1123 <78535378+wzr1123@users.noreply.github.com>
2022-07-18 20:56:20 +0800

    fetch data from mock api

--- src/Pages/RoomSearch/RoomSearch.jsx
+++ src/Pages/RoomSearch/RoomSearch.jsx
@@ -5,58 +5,55 @@ import RoomItem from '../../Components/RoomItem/RoomItem'
 import './RoomSearch.css'
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-const google = window.google;
+
+
 
 const RoomSearch = () => {
+
     const [description, setDescription] = useState("Description");
     const [long, setLong] = useState("103.8198");
     const [lat, setLat] = useState("1.3521");
-    const [map, setMap] = useState();
+    let [map, setMap] = useState();
     const [name, setName] = useState("Hotel Name");
     const [address, setAddress] = useState("Address");
     const [rating, setRating] = useState("Rating");
-    const [amenities_ratings, setAmenities_Ratings] = useState("Amenities_ratings");
+    const [amenities_ratings, setAmenities_Ratings] = useState([]);
     const [amenities, setAmenities] = useState("Amenities");
+    const [image, setImage] = useState("Image Link");
 
-/*    const getQueryParams = (params) => {
-        let query = window.location.search.substring(1);
-        let variable = query.split("&");
-        for (let i = 0; i < variable.length; i++){
-            let split_by_equal_sign = variable[i].split("=")
-            if(split_by_equal_sign[0] == params) return split_by_equal_sign[1]
-        }
-    }*/
-
+    const fetchData = async (hotelID) => {
+        await axios.get(`https://hotelapi.loyalty.dev/api/hotels/${hotelID}`)
+            .then((response) => {
+                setName(response.data.name);
+                setAddress(response.data.address);
+                setDescription(response.data.description);
+                setRating(response.data.rating);
+                setLong(response.data.longitude);
+                setLat(response.data.latitude);
+                //setAmenities(response.data.amenities);
+                setAmenities_Ratings(response.data.amenities_ratings);
+            }).catch(error => console.error(`Error: ${error}`));
+    }
     useEffect(() => {
-        const url = "https://hotelapi.loyalty.dev/api/hotels/${hotelID}";
-        const fetchData = async (hotelID) => {
-            try {
-                const response = await fetch(url);
-                const json = await response.json();
-                console.log(json.name);
-                setName(json.name);
-                setAddress(json.address);
-                setLat(json.latitude);
-                setLong(json.longitude);
-            }catch (error){
-                console.log("error", error)
-            }
-        }
         fetchData("diH7");
     }, []);
 
 
+
 /*    const initMap = () => {
-        const coordinate = new google.map
-    }
-    const render = (status: Status) => {
+        const marker = new google.maps.Marker({
+            position: {lat: lat, lng: long},
+            map: map,
+        })}*/
+
+
+/*    const render = (status: Status) => {
         return <h1>{status}</h1>;
     };
     const Map: React.FC<{}> = () => {};
     const ref = React.useRef(null);
-
-    React.useEffect(() => {
+``
+    useEffect(() => {
         if (ref.current && !map) {
             setMap(new window.google.maps.Map(ref.current, {}));
         }
@@ -76,11 +73,21 @@ const RoomSearch = () => {
         <div className="hotelDetails">
           <div className="hotelDesc">
             <div className="hotelTitle">{name}</div>
-            <div className="hotelDesc">Entire studio • 1 bathroom • 21m² 1 full bed</div>
+            <div className="hotelDesc">
+                <div dangerouslySetInnerHTML={{__html: description}}/>
+                <div>Overall rating: {rating}</div>
+                <div>
+                    <ul>
+                        {amenities_ratings.map(item =>
+                            <li key={item.name}>{item.name}: {item.score}</li>
+                        )}
+                    </ul>
+                </div>
+                <div>Hotel address: {address}</div>
+            </div>
           </div>
           <img src="https://pix10.agoda.net/hotelImages/18391689/0/2c6de0f77a916b78928c57f088f08fc6.jpg?ca=19&ce=1&s=1024x768" className="hotelImg" />
         </div>
-        <p>Should change formatting to how u want it to look, ahhh sorry</p>
         <div className="hotelRooms">
           <RoomItem/>
           <RoomItem/>
