7574e1177b5238ed459baaefb5aba2d7c3b12f09
AnshuGhate <67567787+starrocket2607@users.noreply.github.com>
2022-07-30 17:43:44 +0800

    adding payment file nd options

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ import {
 } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
 import Feature1 from "./Pages/DestinationSearch/DestinationSearch";
-import Feature2 from "./Pages/HotelSearch/HotelSearch"
+import Feature2 from "./Pages/HotelSearch/HotelSearch";
 import Feature3 from "./Pages/RoomSearch/RoomSearch";
 import Feature4 from "./Pages/Booking/Booking";
-
+import Feature4a from "./Pages/payment/Payment";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*<Route path="/" element={<Homepage/>}/>
-        <Route path="/destinationsearch" element={<Feature1/>}/>*/}
-        <Route path="/" element={<Feature1/>}/>
-        <Route path="/hotelsearch/:id" element={<Feature2/>}/>
-        <Route path="/roomsearch/:id" element={<Feature3/>}/>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/destinationsearch" element={<Feature1/>}/>
+        <Route path="/hotelsearch" element={<Feature2/>}/>
+        <Route path="/hotelsearch/:id" element={<Feature3/>}/>
         <Route path="/booking" element={<Feature4/>}/>
+        <Route path="/Payment" element={<Feature4a/>}/>
       </Routes>
     </BrowserRouter>
   );
