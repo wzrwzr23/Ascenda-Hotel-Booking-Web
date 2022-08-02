beffa001058684c839f6fce1c7c6f938090a7b9c
kellieayy <kellie.awyong@gmail.com>
2022-08-02 21:27:27 +0800

    more updates

--- client/src/Pages/Homepage/Homepage.jsx
+++ client/src/Pages/Homepage/Homepage.jsx
@@ -4,20 +4,26 @@ import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer';
 import Featured from '../../Components/Featured/Featured';
 import React from 'react'
+import ImageSlide from '../../Components/ImageSlider/ImageSlide';
+import DestinationSearch from '../DestinationSearch/DestinationSearch';
 
 const Homepage = () => {
   return (
     <>
-    <Navbar/>
-    <Header/>
-    <br />
-    <div className="homeContainer">
-        <Featured/>
+    <div id='homewrap'>
+      <Navbar/>
+      <Header/>
+      <ImageSlide/>
+      <DestinationSearch/>
+      {/* <div id='featured'><Featured/></div> */}
+      {/* <div className="homeContainer">
+          <Featured/>
+      </div> */}
+      {/* <h1>Display1</h1>
+      <h1>Display2</h1>
+      <h1>Display3</h1> */}
+      <Footer/>
     </div>
-    <h1>Display1</h1>
-    <h1>Display2</h1>
-    <h1>Display3</h1>
-    <Footer/>
     </>
   );
 }
---
+++ client/src/secretKey.js
@@ -0,0 +1,1 @@
+export default "encryptk3y1234"
