8f17a951a2d0064e41705c0197e3ce75c5295434
AnshuGhate <67567787+starrocket2607@users.noreply.github.com>
2022-08-03 07:03:56 +0800

    Update Boking.test.jsx

--- __test__/Boking.test.jsx
+++ __test__/Boking.test.jsx
@@ -1,1 +1,80 @@
-ddfc
+import React from "react";
+import {unmountComponentAtNode} from "react-dom";
+import {act} from "react-dom/test-utils";
+import Booking from "./Booking";
+import TestRenderer from 'react-test-renderer';
+import Navbar from "../../Components/Navbar/Navbar";
+import { render, fireEvent, screen } from "@testing-library/react";
+
+
+let container = null;
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("renders Booking", async () => {
+
+    const Bookingtest = {
+        destinationID: "destId",
+        hotelID: "hotelId",
+        bookingInfo: {
+          noNight: 2,
+          startDate: "checkInDate",
+          endDate: "checkOutDate",
+          noAdult: 2,
+          noChildren: 2,
+          roomType: "hotelRoomType",
+          
+        },
+        
+        guestInformation: {
+          userID: "uid",
+          salutation: "Ms",
+          firstName: "firstName",
+          lastName: "lastName",
+          email: "yu@gmail.com",
+          phone: 12345678,
+          country: "country",
+        },
+        payeeInformation: {
+          paymentID: "",
+          payeeID: "uid",
+        },
+        uid: "EdPDTW6cmVhsBICgZNYWxHCPIDi2",
+      };
+      
+    
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+        Promise.resolve({
+            json: () => Promise.resolve(Bookingtest)
+        })
+    );
+    const testRenderer = TestRenderer.create(<Booking/>)
+    const testInstance = testRenderer.root;
+
+    expect(testInstance.findByProps({className: "booking"})).toContain(<Navbar/>);
+
+    // Use the asynchronous version of act to apply resolved promises
+    await act(function Component() {
+        render(<RoomSearch hotelID="0KmN"/>, container);
+    });
+
+    expect(container.querySelector("div").textContent).toBe(Bookingtest.destinationID);
+    expect(container.textContent).toContain(Bookingtest.hotelID);
+    expect(container.textContent).toContain(Bookingtest.userID);
+    expect(container.textContent).toContainHTML(Bookingtest.description);
+
+    // remove the mock to ensure tests are completely isolated
+    global.fetch.mockRestore();
+});
+
+
